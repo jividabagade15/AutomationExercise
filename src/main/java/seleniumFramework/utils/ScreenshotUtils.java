2f61bdd00139452d52c300a8c7a339aad5247654
Jivida bagade <jivibagade.2415@gmail.com>
2026-07-17 09:42:28 +0530

    Addition of Screenshot Utility

---
+++ src/main/java/seleniumFramework/utils/ScreenshotUtils.java
@@ -0,0 +1,21 @@
+package seleniumFramework.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class ScreenshotUtils {
+	public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
+		FileUtils.copyFile(source, file);
+
+		return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
+	}
+
+}
